b40d8c63ff3d40c685a312e79ec2af7a9ad28383
Argo Roots <argo.roots@lhv.ee>
2018-04-15 07:26:13 +0300

    Make Jukebox class

--- player.py
+++ player.py
@@ -16,73 +16,102 @@ GPIO.setwarnings(False)
 GPIO.cleanup()
 
 
-card = None
-song = 0
-player = None
-playlist = []
-
-cards = yaml.load(open('cards.yaml'))
-
-
-def read_rfid():
-    reader = MFRC522.MFRC522()
-
-    (status, TagType) = reader.MFRC522_Request(reader.PICC_REQIDL)
-    if status != reader.MI_OK:
-        return None
-
-    (status, uid) = reader.MFRC522_Anticoll()
-    if status != reader.MI_OK:
-        return None
-
-    n = 0
-    for i in range(0, 5):
-        n = n * 256 + uid[i]
-    return n
+class Jukebox:
+    def __init__(self, cards_file='cards.yaml'):
+        self.player = None
+        self.cards = yaml.load(open('cards.yaml'))
+        self.card = None
+        self.playlist = []
+        self.song = 0
 
 
-print('Ready to play')
-while(1):
-    try:
-        rfid = read_rfid()
+    def read_rfid(self):
+        reader = MFRC522.MFRC522()
 
-        if not rfid and card:
-            player.stdin.write('q')
-            card = None
-            print('Stop')
+        (status, TagType) = reader.MFRC522_Request(reader.PICC_REQIDL)
+        if status != reader.MI_OK:
+            return None
 
-        if rfid and rfid != card:
-            card = rfid
-            song = 0
-            playlist = []
+        (status, uid) = reader.MFRC522_Anticoll()
+        if status != reader.MI_OK:
+            return None
 
-            if rfid not in cards:
-                print('Unknown card: %s' % rfid)
-            else:
-                print('Card: %s' % rfid)
+        n = 0
+        for i in range(0, 5):
+            n = n * 256 + uid[i]
+        return n
 
-                if cards[rfid].startswith('http'):
-                    playlist.append(cards[rfid])
+
+    def load_card(self, rfid):
+        if self.card == rfid:
+            return
+
+        if rfid not in self.cards:
+            print('Unknown card %s' % rfid)
+            return
+
+        self.card = rfid
+        self.song = 0
+        self.playlist = []
+        card_path = self.cards[self.card]
+
+        print('Card %s' % self.card)
+
+        if card_path.startswith('http'):
+            self.playlist.append(card_path)
+        else:
+            for infile in glob.glob(card_path):
+                self.playlist.append(infile)
+
+            if len(self.playlist) == 0:
+                print('No files to play')
+                return
+
+        self.playlist.sort()
+
+        return True
+
+
+    def play_file(self, song=None):
+        if song == None:
+            self.song = self.song + 1
+            if self.song > len(self.playlist):
+                self.song = 0
+        else:
+            self.song = song
+
+        mp3_path = self.playlist[self.song]
+        print('Play: %s' % mp3_path)
+        self.player = subprocess.Popen(['omxplayer', mp3_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+    def stop_player(self):
+        if not self.card:
+            return
+
+        self.card = None
+        self.player.stdin.write('q')
+
+        print('Stop')
+
+
+    def start(self):
+        while(1):
+            try:
+                rfid = self.read_rfid()
+
+                if rfid:
+                    if self.load_card(rfid):
+                        self.play_file(0)
+                    if self.player.poll() == 0:
+                        self.play_file()
                 else:
-                    for infile in glob.glob(cards[rfid]):
-                        playlist.append(infile)
-                playlist.sort()
+                    self.stop_player()
 
-                mp3_path = playlist[song]
-                print('Play: %s' % mp3_path)
-                player = subprocess.Popen(['omxplayer', mp3_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        if card and player.poll() == 0:
-            song = song + 1
-            if song > len(playlist):
-                song = 0
-
-            mp3_path = playlist[song]
-            print('Play: %s' % mp3_path)
-            player = subprocess.Popen(['omxplayer', mp3_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                time.sleep(1)
+            except Exception as err:
+                print(str(err))
 
 
-    except Exception as err:
-        print(str(err))
-
-    time.sleep(1)
+if __name__ == '__main__':
+    Jukebox('cards.yaml').start()
